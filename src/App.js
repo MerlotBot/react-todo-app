d1d826231eda08f4fb3eb162ecf3c2de94a7caac
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-07-23 08:25:36 +0200

    added todo item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import Todos from "../src/components/Todos";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Go to the gym",
+        completed: false
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
